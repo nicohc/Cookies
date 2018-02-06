c9ae1a9638abc332d261897ed18ebf9aeffc6859
Nico <nico1099@hotmail.fr>
2018-02-06 18:54:24 +0100

    Sessions avec edit fonctionnelet restriction d'acces

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,14 +25,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    if @user.id != current_user.id
-      flash[:danger] = 'Accès refusé !'
-      redirect_to root_path
-    end
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user.id != current_user.id
+      flash[:danger] = 'Accès refusé !'
+      redirect_to root_path
+    end
   end
 
   def update
